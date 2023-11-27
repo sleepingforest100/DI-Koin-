7b1011e65177b5554bb4574d10b88168ccabfae1
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-11-27 12:09:57 +0600

    initial  commit

--- app/src/main/java/kz/just_code/di_koin/data/MainViewModel.kt
+++ app/src/main/java/kz/just_code/di_koin/data/MainViewModel.kt
@@ -1,12 +1,57 @@
 package kz.just_code.di_koin.data
 
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kz.just_code.di_koin.data.useCases.GetCurrentWeatherUseCase
 import kz.just_code.di_koin.data.useCases.SearchWeatherUseCase
 
 class MainViewModel(
     private val getCurrentWeather: GetCurrentWeatherUseCase,
     private val searchWeather: SearchWeatherUseCase
-): ViewModel() {
+) : BaseViewModel() {
+    fun getCurrentWeather(city: String) {
+        launch(
+            request = {
+                getCurrentWeather.execute(city)
+            },
+            onSuccess = {
 
+            }
+        )
+    }
+}
+
+abstract class BaseViewModel : ViewModel() {
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + Job())
+
+    private var _loadingLiveData = MutableLiveData<Boolean>()
+    val loadingLiveData: LiveData<Boolean> = _loadingLiveData
+
+    private var _exceptionLiveData = MutableLiveData<String?>()
+    val exceptionLiveData: LiveData<String?> = _exceptionLiveData
+
+    fun <T> launch(
+        request: suspend () -> T,
+        onSuccess: (T) -> Unit = { }
+    ) {
+        coroutineScope.launch {
+            try {
+                _loadingLiveData.postValue(true)
+                val response = request.invoke()
+                onSuccess.invoke(response)
+            } catch (e: Exception) {
+                _exceptionLiveData.postValue(e.message)
+                Log.e(">>>", e.message.orEmpty())
+            } finally {
+                _loadingLiveData.postValue(false)
+            }
+        }
+    }
 }
